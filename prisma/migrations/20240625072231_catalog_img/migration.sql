040e7c1875c926932c316d113e46c42212e3274f
xuhl <xuhl@citycloud.com.cb>
2024-06-25 15:25:29 +0800

    polish: 类目和品牌新增logo

---
+++ prisma/migrations/20240625072231_catalog_img/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `Brand` ADD COLUMN `logo` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `Catalog` ADD COLUMN `imgUrl` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `SubCatalog` ADD COLUMN `imgUrl` VARCHAR(191) NULL;
